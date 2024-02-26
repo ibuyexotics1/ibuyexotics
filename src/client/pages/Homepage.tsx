8c087ee5bed9c13343c1e62ee6120b78fc503b56
degirro <deqirohdquality@gmail.com>
2024-02-26 17:50:18 -0500

    added state for for sidebar activation and sidebar component

--- src/client/pages/Homepage.tsx
+++ src/client/pages/Homepage.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import { Form } from "../components/form/vin-form/Form";
 import { Hero } from "../components/hero/Hero";
 import { Topbar } from "../components/topbar/Topbar";
 import { HomeContainer } from "../utils/styles/home/HomeStyled";
+import { Sidebar } from "../components/sidebar/Sidebar";
 
 export const Homepage = () => {
+  const [isActive, setActive] = useState<boolean>(false);
+
+  const handleActive = () => {
+    setActive((prev: boolean) => !prev);
+  };
+
   return (
     <HomeContainer>
+      {isActive && <Sidebar />}
+      <Topbar handleActive={handleActive} />
       <Hero />
       <Form />
     </HomeContainer>
