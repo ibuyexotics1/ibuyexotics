2f76aa186c247f3a81163ec04f19398ff6fb4a7a
degirro <deqirohdquality@gmail.com>
2024-02-26 17:50:37 -0500

    variants for sidebar nav text animations

---
+++ src/client/utils/variants.ts
@@ -0,0 +1,18 @@
+export const navBackTextVariants = {
+    active: {
+      y: 0,
+    },
+    inactive: {
+      y: '100%',
+    },
+};
+
+export const navTextVariants = {
+    active: {
+      y: '-100%',
+    },
+    inactive: {
+      y: 0,
+    },
+};
+  
