bacb7ef40e3e2f14d0c57a51d594e0597d54692a
degirro <deqirohdquality@gmail.com>
2024-02-27 04:34:44 -0500

    added faq data

--- src/client/components/faq/data/FaqData.ts
+++ src/client/components/faq/data/FaqData.ts
@@ -7,27 +7,27 @@ export type FaqObj = {
 export const FaqData: FaqObj[] = [
     {
         id: 1,
-        question: 'Lorem ipsum?',
-        answer: 'Lorem ipsum.'
+        question: 'How do I sell my exotic vehicle?',
+        answer: 'Complete the form. We will reach out to you with all the information you need to sell it.'
     },
     {
         id: 2,
-        question: 'Lorem ipsum?',
-        answer: 'Lorem ipsum.'
+        question: 'What if my exotic vehicle is being leased or financed?',
+        answer: 'No problem! We buyout/payoff leased or financed exotic vehicles.'
     },
     {
         id: 3,
-        question: 'Lorem ipsum?',
-        answer: 'Lorem ipsum.'
+        question: 'How long will it take for my vehicle to be paid off if I’m leasing or financing it?',
+        answer: 'We will reach out to your bank as soon as possible. We need the title as badly as you want to pay off the vehicle.'
     },
     {
         id: 4,
-        question: 'Lorem ipsum?',
-        answer: 'Lorem ipsum.'
+        question: 'Where are you located?',
+        answer: 'We are located in Staten Island, New York.'
     },
     {
         id: 5,
-        question: 'Lorem ipsum?',
-        answer: 'Lorem ipsum.'
+        question: 'How can I contact you?',
+        answer: 'Shoot us an email with your contact information and we will reach out to you. Email: i-buy-exotics1@gmail.com'
     },
 ]
