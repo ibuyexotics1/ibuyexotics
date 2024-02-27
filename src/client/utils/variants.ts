6da6d6993596321ec90b6ef8e6629147c45e8d14
degirro <deqirohdquality@gmail.com>
2024-02-27 04:08:25 -0500

    added variants for animations

--- src/client/utils/variants.ts
+++ src/client/utils/variants.ts
@@ -1,18 +1,48 @@
 export const navBackTextVariants = {
-    active: {
-      y: 0,
-    },
-    inactive: {
-      y: '100%',
-    },
+  active: {
+    y: 0,
+  },
+  inactive: {
+    y: '100%',
+  },
 };
 
 export const navTextVariants = {
-    active: {
-      y: '-100%',
-    },
-    inactive: {
-      y: 0,
-    },
+  active: {
+    y: '-100%',
+  },
+  inactive: {
+    y: 0,
+  },
 };
-  
+
+export const faqVariants = {
+  open: {
+    height: '70px',
+  },
+  closed: {
+    height: '0px',
+  },
+};
+
+export const burgerLineTopVariants = {
+  open: {
+    translateY: 4,
+    rotateZ: 45,
+  },
+  closed: {
+    translateY: 0,
+    rotateZ: 0,
+  },
+};
+
+export const burgerLineBotVariants = {
+  open: {
+    translateY: -4,
+    rotateZ: -45,
+  },
+  closed: {
+    translateY: 0,
+    rotateZ: 0,
+  },
+};
