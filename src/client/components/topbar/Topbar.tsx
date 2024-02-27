203fbb78deb33d95ff7c3dadc3c9b20649219b65
degirro <deqirohdquality@gmail.com>
2024-02-27 04:07:40 -0500

    added top bar burger and animations

--- src/client/components/topbar/Topbar.tsx
+++ src/client/components/topbar/Topbar.tsx
@@ -1,19 +1,33 @@
+import { easeInOut } from "framer-motion";
 import {
   BurgerLine,
+  BurgerLine2,
   BurgerWrapper,
   TopbarContainer,
 } from "../../utils/styles/topbar/TopbarStyled";
+import { burgerLineBotVariants, burgerLineTopVariants } from "../../utils/variants";
 
 type props = {
   handleActive: () => void;
+  isActive: boolean;
 };
 
-export const Topbar: React.FC<props> = ({ handleActive }) => {
+export const Topbar: React.FC<props> = ({ handleActive, isActive }) => {
   return (
     <TopbarContainer>
       <BurgerWrapper onClick={handleActive}>
-        <BurgerLine />
-        <BurgerLine />
+        <BurgerLine
+          initial={{ translateY: 0, rotateZ: 0 }}
+          variants={burgerLineTopVariants}
+          animate={isActive ? 'open' : 'closed'}
+          transition={{ duration: 0.3, ease: easeInOut }}
+        />
+        <BurgerLine2
+          initial={{ translateY: 0, rotateZ: 0 }}
+          variants={burgerLineBotVariants}
+          animate={isActive ? 'open' : 'closed'}
+          transition={{ duration: 0.3, ease: easeInOut }}
+        />
       </BurgerWrapper>
     </TopbarContainer>
   );
