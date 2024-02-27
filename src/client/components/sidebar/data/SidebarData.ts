2d9a26268eb1402d2429331b9bcb0df1f68a8bc9
degirro <deqirohdquality@gmail.com>
2024-02-27 05:27:14 -0500

    created sidebar data

---
+++ src/client/components/sidebar/data/SidebarData.ts
@@ -0,0 +1,16 @@
+import { FaqDataType } from "../../../utils/types";
+
+export const sidebarNav: FaqDataType[] = [
+    {
+        link: '#about',
+        name: 'About'
+    },
+    {
+        link: '#vin',
+        name: 'Vin Contact'
+    },
+    {
+        link: '#faq',
+        name: 'Faq'
+    },
+]
