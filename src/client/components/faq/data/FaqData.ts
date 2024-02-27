6cb3901f1f295ac9cfe6c54d6dad216513a8c3b1
degirro <deqirohdquality@gmail.com>
2024-02-27 04:05:39 -0500

    added faq data

---
+++ src/client/components/faq/data/FaqData.ts
@@ -0,0 +1,33 @@
+export type FaqObj = {
+    id: number;
+    question: string;
+    answer: string;
+}
+
+export const FaqData: FaqObj[] = [
+    {
+        id: 1,
+        question: 'Lorem ipsum?',
+        answer: 'Lorem ipsum.'
+    },
+    {
+        id: 2,
+        question: 'Lorem ipsum?',
+        answer: 'Lorem ipsum.'
+    },
+    {
+        id: 3,
+        question: 'Lorem ipsum?',
+        answer: 'Lorem ipsum.'
+    },
+    {
+        id: 4,
+        question: 'Lorem ipsum?',
+        answer: 'Lorem ipsum.'
+    },
+    {
+        id: 5,
+        question: 'Lorem ipsum?',
+        answer: 'Lorem ipsum.'
+    },
+]
