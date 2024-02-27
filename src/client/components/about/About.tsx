386c4cd76d485979bc97844e503d65abdd319d8c
degirro <deqirohdquality@gmail.com>
2024-02-27 03:18:08 -0500

    created about component

--- src/client/components/about/About.tsx
+++ src/client/components/about/About.tsx
@@ -1,9 +1,17 @@
-import { AboutContainer } from "../../utils/styles/about/AboutStyled"
+import { AboutContainer, AboutContent, AboutSubTitle, AboutText, AboutTitle, AboutTitleWrapper, AboutWrapper } from "../../utils/styles/about/AboutStyled"
 
 export const About = () => {
     return (
         <AboutContainer>
-            
+            <AboutWrapper>
+                <AboutTitleWrapper>
+                    <AboutTitle>About Us</AboutTitle>
+                    <AboutSubTitle>Learn about our service</AboutSubTitle>
+                </AboutTitleWrapper>
+                <AboutContent>
+                    <AboutText>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde quas doloremque deserunt id ad dolore, at exercitationem, aperiam ab ipsam assumenda sit. Nisi expedita adipisci sint illo rerum, quae nesciunt?</AboutText>
+                </AboutContent>
+            </AboutWrapper>
         </AboutContainer>
     )
 }
