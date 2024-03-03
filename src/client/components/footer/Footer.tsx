856fbe22628267ead11f010c53cdefe7df6d13eb
degirro <deqirohdquality@gmail.com>
2024-03-03 01:55:40 -0500

    initial footer

---
+++ src/client/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import {
+  FooterContainer,
+  FooterCopyright,
+} from "../../utils/styles/footer/FooterStyled";
+
+export const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterCopyright>© 2024 I Buy Exotics</FooterCopyright>
+    </FooterContainer>
+  );
+};
