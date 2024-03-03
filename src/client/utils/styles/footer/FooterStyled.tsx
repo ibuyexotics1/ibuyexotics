e7438c3af1991f9e9d505805c734eb0e082aadd4
degirro <deqirohdquality@gmail.com>
2024-03-03 01:56:47 -0500

    initial footer styling

---
+++ src/client/utils/styles/footer/FooterStyled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 70px;
+  padding: 0px 20px;
+  background: rgb(8, 8, 8);
+`;
+
+export const FooterCopyright = styled.p`
+  font-size: 15px;
+  color: white;
+`;
