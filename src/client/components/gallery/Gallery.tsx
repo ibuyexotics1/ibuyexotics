37a4627bd31f91cf7dc17122fa7379d3f25814c4
degirro <deqirohdquality@gmail.com>
2024-03-03 01:28:15 -0500

    added lightbox

--- src/client/components/gallery/Gallery.tsx
+++ src/client/components/gallery/Gallery.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 import {
   GalleryContainer,
   GalleryImgContainer,
@@ -5,15 +7,38 @@ import {
 } from "../../utils/styles/gallery/GalleryStyled";
 import { GalleryDataType } from "../../utils/types";
 import { GalleryData } from "./data/GalleryData";
+import { Lightbox } from "./lightbox/Lightbox";
 
 export const Gallery = () => {
+  const [selectedImg, setSelectedImg] = useState<string>("");
+
+  const handleLightbox = (imgSrc: string) => {
+    setSelectedImg(imgSrc);
+  };
+
+  const handleClose = () => {
+    setSelectedImg("");
+  };
+
   return (
-    <GalleryContainer>
-      <GalleryWrapper>
-        {GalleryData.map((img: GalleryDataType) => (
-          <GalleryImgContainer key={img.imgSrc} imgSrc={img.imgSrc} />
-        ))}
-      </GalleryWrapper>
-    </GalleryContainer>
+    <>
+      <GalleryContainer>
+        <GalleryWrapper>
+          {GalleryData.map((img: GalleryDataType) => (
+            <GalleryImgContainer
+              key={img.imgSrc}
+              imgSrc={img.imgSrc}
+              onClick={() => handleLightbox(img.imgSrc)}
+            />
+          ))}
+        </GalleryWrapper>
+      </GalleryContainer>
+
+      <AnimatePresence>
+        {selectedImg && (
+          <Lightbox handleClose={handleClose} selectedImg={selectedImg} />
+        )}
+      </AnimatePresence>
+    </>
   );
 };
