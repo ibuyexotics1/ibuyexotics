61076fe9e274898fd3112899deb571507bc89ee7
degirro <deqirohdquality@gmail.com>
2024-03-03 00:39:32 -0500

    intial  gallery data

---
+++ src/client/components/gallery/data/GalleryData.ts
@@ -0,0 +1,34 @@
+import { GalleryDataType } from "../../../utils/types";
+
+export const GalleryData: GalleryDataType[] = [
+    {
+        imgSrc: '/gallery/gallery-1.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-2.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-3.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-4.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-5.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-6.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-7.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-8.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-9.jpg'
+    },
+    {
+        imgSrc: '/gallery/gallery-10.jpg'
+    },
+]
