e0c8a4eaa0d1de7218c321030f0b2e149b1ace88
degirro <deqirohdquality@gmail.com>
2024-03-03 20:43:25 -0500

    hero styling

--- src/client/utils/styles/hero/HeroStyled.tsx
+++ src/client/utils/styles/hero/HeroStyled.tsx
@@ -42,6 +42,7 @@ export const HeroContent = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 8;
+  gap: 20px;
 `;
 
 export const HeroGradient = styled.div`
@@ -57,39 +58,6 @@ export const HeroGradient = styled.div`
   );
 `;
 
-export const HeroSubtitleContainer = styled.div`
-  display: flex;
-  gap: 20px;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const SubtitleLine = styled.div`
-  height: 2px;
-  width: 70px;
-  background: rgb(250, 250, 250, 0.2);
-
-  @media (max-width: 1024px) {
-    width: 30px;
-  }
-`;
-
-export const HeroSubtitle = styled.p`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
-  color: gold;
-  font-family: "DM Serif Display", serif;
-  text-transform: uppercase;
-  letter-spacing: 5px;
-  font-weight: 400;
-
-  @media (max-width: 1024px) {
-    font-size: 15px;
-  }
-`;
-
 export const HeroButton = styled.button`
   border: 1px solid rgb(250, 250, 250, 0.5);
   font-size: 20px;
@@ -99,7 +67,6 @@ export const HeroButton = styled.button`
   letter-spacing: 5px;
   font-weight: 800;
   background: none;
-  margin-top: 50px;
   padding: 20px 40px;
   cursor: pointer;
   transition: 0.3s all ease-in-out;
