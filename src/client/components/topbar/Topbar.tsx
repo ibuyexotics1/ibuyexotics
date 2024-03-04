d8aa06e99b72c194fcb56871bc8a8c8f27270fcc
degirro <deqirohdquality@gmail.com>
2024-03-03 20:42:32 -0500

    added logo to topbar

--- src/client/components/topbar/Topbar.tsx
+++ src/client/components/topbar/Topbar.tsx
@@ -3,9 +3,14 @@ import {
   BurgerLine,
   BurgerLine2,
   BurgerWrapper,
+  LogoWrapper,
   TopbarContainer,
+  TopbarLogo,
 } from "../../utils/styles/topbar/TopbarStyled";
-import { burgerLineBotVariants, burgerLineTopVariants } from "../../utils/variants";
+import {
+  burgerLineBotVariants,
+  burgerLineTopVariants,
+} from "../../utils/variants";
 
 type props = {
   handleActive: () => void;
@@ -15,17 +20,20 @@ type props = {
 export const Topbar: React.FC<props> = ({ handleActive, isActive }) => {
   return (
     <TopbarContainer isActive={isActive}>
+      <LogoWrapper href="/">
+        <TopbarLogo src="logo.svg" />
+      </LogoWrapper>
       <BurgerWrapper onClick={handleActive}>
         <BurgerLine
           initial={{ translateY: 0, rotateZ: 0 }}
           variants={burgerLineTopVariants}
-          animate={isActive ? 'open' : 'closed'}
+          animate={isActive ? "open" : "closed"}
           transition={{ duration: 0.3, ease: easeInOut }}
         />
         <BurgerLine2
           initial={{ translateY: 0, rotateZ: 0 }}
           variants={burgerLineBotVariants}
-          animate={isActive ? 'open' : 'closed'}
+          animate={isActive ? "open" : "closed"}
           transition={{ duration: 0.3, ease: easeInOut }}
         />
       </BurgerWrapper>
